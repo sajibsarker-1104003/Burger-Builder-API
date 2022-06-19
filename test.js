0bba6ca0d7fa9d4d83c3ed8c7989a91c2c470d8a
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-19 19:21:36 +0600

    Testing Our BackEnd API

--- test.js
+++ test.js
@@ -13,3 +13,10 @@ const user={
 const {error}=schema.validate(user);
 
 console.log(error.details[0].message);
+
+
+//for testing backend API
+
+//sajibsarker0111@gmail.com=>token=> eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmFmMWY5ZDlhY2ExMmU4N2I5N2UxOTUiLCJlbWFpbCI6InNhamlic2Fya2VyMDExMUBnbWFpbC5jb20iLCJpYXQiOjE2NTU2NDQwNjEsImV4cCI6MTY1NTY1NDg2MX0.LNwFGT6mi_VjJIrVPR7Wxmjxfwuv_uVjEcvyUwyYX5w
+
+// a@gmail.com=>token=>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmFmMjA2NTlhY2ExMmU4N2I5N2UxOTgiLCJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWF0IjoxNjU1NjQ0MjYxLCJleHAiOjE2NTU2NTUwNjF9.3Ewz17ECF2ayZQ2SiEckgXOuUjz6JZc4pKPDR5Efydg  =>userId=>62af20659aca12e87b97e198
